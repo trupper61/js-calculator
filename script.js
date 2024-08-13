ecb8e75d6d2b8d8a557eb310d48bcc19afdc575a
Rupert <rupert.hoffmann.06@gmail.com>
2024-08-13 23:40:01 +0200

    Added logic for addition, substraction, multiplication and division

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function addition(a, b){
+    return a + b;
+}
+function substraction(a, b){
+    return a - b;
+}
+function multiplication(a, b){
+    return a * b;
+}
+function division(a, b){
+    if (b === 0){
+        return "ERROR";
+    } else{
+        return a / b;
+    }
+}
