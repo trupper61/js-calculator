8acf7a3ed6616b45da5d58ed55a1db00123fde74
Rupert <rupert.hoffmann.06@gmail.com>
2024-08-13 23:49:46 +0200

    Added operate function to distribute the operations

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let num1 = 0;
+let operator;
+let num2 = 0;
 
 function addition(a, b){
     return a + b;
@@ -13,5 +16,23 @@ function division(a, b){
         return "ERROR";
     } else{
         return a / b;
+    }
+}
+function operate(numa, operator, numb){
+    switch(operator){
+        case "+":
+            addition(numa, numb);
+            break;
+        case "-":
+            substraction(numa, numb);
+            break;
+        case "*":
+            multiplication(numa, numb);
+            break;
+        case "/":
+            division(numa, numb);
+        default:
+            console.log("ERROR! wrong operator");
+            break;
     }
 }
